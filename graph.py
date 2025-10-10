524d476b3accb254d5831cff24ec165ec32b8b54
stevee0611 <sardorerkinoff0611@gmail.com>
2025-10-10 02:03:27 -0400

    Langgraph project

---
+++ graph.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+from langgraph.graph import MessagesState
+from langchain_core.messages import HumanMessage, SystemMessage
+import textwrap
+
+def assistant(state: MessagesState):
+    sys_msg = SystemMessage(content="You are Sardor's personal assistant for learning to code")
+    response = llm.invoke([sys_msg] + state['messages'])
+    return {'messages': response}
+from langgraph.graph import START, StateGraph, END
+from langgraph.prebuilt import tools_condition, ToolNode
+from IPython.display import Image, display
+from langgraph.checkpoint.memory import MemorySaver
+memory = MemorySaver()
+config = {"configurable": {"thread_id": "1"}}
+
+builder = StateGraph(MessagesState)
+builder.add_node('chat', assistant)
+builder.add_edge(START, 'chat')
+builder.add_edge('chat', END)
+graph = builder.compile(checkpointer=memory)
+
+conversation = [HumanMessage('What is my name and what do you recommend me to learn today?')]
+conversation = graph.invoke({"messages": conversation}, config)
+for m in conversation["messages"]:
+    print(f"=== {m.type.capitalize()} Message ===")
+    for line in textwrap.wrap(m.content, width=80):
+        print(line)
+    print()
